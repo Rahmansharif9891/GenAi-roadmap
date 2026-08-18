b0eba110f22718c4419f2198bd857564c0425e1e
Rahmansharif9891 <RAHMANSHARIF9891gmail.com>
2026-08-18 20:36:08 +0530

    Add Python basics tasks

---
+++ 01-Python/01-Basics/c.py
@@ -0,0 +1,5 @@
+age = int(input("ENter the age : " ))
+if (age >= 18):
+    print("Eligible to vote:")
+else:
+    print("Not Eligiable:")
---
+++ 01-Python/01-Basics/d.py
@@ -0,0 +1,19 @@
+from re import A
+
+
+number = int(input("Enter the marks : "))
+
+if (number >= 90):
+    print("A+")
+
+elif (number >=  80):
+    print("A")
+
+elif (number >= 6850):
+    print("B")
+
+elif (number >= 40):
+    print("C")
+
+else:
+    print("Fail")
